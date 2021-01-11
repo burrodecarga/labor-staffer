58275b9ba0f33015205284d54150e851a7afbf4a
burrodecarga <edwinhenriquezh@hotmail.com>
2021-01-11 06:58:10 -0400

    Primeros pasos

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,22 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import botton from '../components/Button.js'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>Labor Staffer</title>
         <link rel="icon" href="assets/logo/icon_laucher.png" />
+
       </Head>
       <main className={styles.main}>
+
         <h1 className={styles.title}>
           Labor-Staffer
         </h1>
-
+         <button className={styles.button}>Login with linkdin</button>
         </main>
 
       <footer className={styles.footer}>
